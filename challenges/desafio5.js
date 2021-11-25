e7aff54363d70ec72f8f5017e1abab840f3a3ddf
Murilo Ribeiro <murilorsv14@gmail.com>
2021-11-25 17:41:19 -0300

    desafio 1 a 6

---
+++ challenges/desafio5.js
@@ -0,0 +1,33 @@
+const favoriteActors = [
+  "Sandra Bullock",
+  "Tom Hanks",
+  "Julia Roberts",
+  "Kevin Spacey",
+  "George Clooney",
+];
+
+db.movies.aggregate(
+  [
+    { $match:
+      {
+        countries: { $all: ["USA"] },
+        "tomatoes.viewer.rating": { $gte: 3 },
+        cast: { $in: favoriteActors },
+      },
+    },
+    { $project:
+      {
+        num_favs: { $size: { $setIntersection: ["$cast", favoriteActors] } },
+        title: 1,
+        _id: 0,
+        "tomatoes.viewer.rating": 1,
+      },
+    },
+    { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
+    {
+      $project: { num_favs: 0 },
+    },
+    { $skip: 24 },
+    { $limit: 1 },
+  ],
+);
