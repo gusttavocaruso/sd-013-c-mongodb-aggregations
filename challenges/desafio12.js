86869a5ca71d6832be1ae0786c0a795902d4a246
Guilherme da Costa <guilhermedc93@gmail.com>
2021-11-26 14:23:22 -0300

    conclui requisito 12

---
+++ challenges/desafio12.js
@@ -0,0 +1,27 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: {
+        dia: {
+          $dayOfWeek: "$startTime",
+        },
+        estacao: "$startStationName",
+      },
+      total: { $sum: 1 },
+
+    },
+  },
+  {
+    $sort: { total: -1 },
+  },
+  {
+    $limit: 1,
+  },
+  {
+    $project: {
+      _id: false,
+      nomeEstacao: "$_id.estacao",
+      total: "$total",
+    },
+  },
+]);
