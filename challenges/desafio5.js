0205cb9dc05f973467a12fbd47e1eb95fe5fcaaa
José Demeneghi <josedemeneghi_cba@hotmail.com>
2021-11-26 12:35:27 -0400

    requisito 5

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -3,16 +3,16 @@ const actors = ["Sandra Bullock", "Tom Hanks", "Julia Roberts", "Kevin Spacey", 
 db.movies.aggregate([
   { $match:
     {
-      countries: { $in: ["USA"] },
+      countries: { $all: ["USA"] },
       "tomatoes.viewer.rating": { $gte: 3 },
-      cast: { $in: [actors] },
+      cast: { $in: actors },
     },
   },
   { $addFields: { num_favs: { $size: { $setIntersection: ["$cast", actors] } } } },
   { $sort: { num_favs: -1, "tomatoes.viewer.rating": -1, title: -1 } },
-  { $project: { _id: false, title: true } },
   { $skip: 24 },
   { $limit: 1 },
+  { $project: { _id: 0, title: 1 } },
 ]);
 // https://docs.mongodb.com/manual/reference/operator/aggregation/setIntersection/
 // https://docs.mongodb.com/manual/reference/operator/aggregation/size/
