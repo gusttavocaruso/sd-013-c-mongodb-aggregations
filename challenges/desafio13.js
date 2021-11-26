bac4de83d906c4d20bd07a3fe13ed0ef5ff3b449
Guilherme da Costa <guilhermedc93@gmail.com>
2021-11-26 14:23:29 -0300

    conclui requisito 13

---
+++ challenges/desafio13.js
@@ -0,0 +1,26 @@
+db.trips.aggregate([
+  {
+    $match: {
+      startTime: { $gte: ISODate("2016-03-10") },
+    },
+  },
+  {
+    $group: {
+      _id: null,
+      duracaoMediaEmMinutos: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            1000 * 60,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $project: {
+      _id: false,
+      duracaoMediaEmMinutos: { $ceil: "$duracaoMediaEmMinutos" },
+    },
+  },
+]);
