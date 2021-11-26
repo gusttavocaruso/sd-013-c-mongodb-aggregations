6d12ecf9cbb150bfbb3440dfafeb30c522979e8b
Guilherme da Costa <guilhermedc93@gmail.com>
2021-11-26 14:23:47 -0300

    conclui requisito 14

---
+++ challenges/desafio14.js
@@ -0,0 +1,28 @@
+db.trips.aggregate([
+  {
+    $group: {
+      _id: "$bikeid",
+      duracaoMedia: {
+        $avg: {
+          $divide: [
+            { $subtract: ["$stopTime", "$startTime"] },
+            1000 * 60,
+          ],
+        },
+      },
+    },
+  },
+  {
+    $sort: { duracaoMedia: -1 },
+  },
+  {
+    $limit: 5,
+  },
+  {
+    $project: {
+      _id: false,
+      bikeId: "$_id",
+      duracaoMedia: { $ceil: "$duracaoMedia" },
+    },
+  },
+]);
