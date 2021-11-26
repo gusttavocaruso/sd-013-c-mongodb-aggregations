adbb4ae76b1299c0ed7b20c6ca60ff1c79df82ef
Marcus Vinicius Mendes do Nascimento <marcus.mendes89@gmail.com>
2021-11-26 04:05:00 -0300

    Requisito 4 concluido

---
+++ challenges/desafio4.js
@@ -0,0 +1,10 @@
+db.movies.aggregate([
+  { $project: {
+    title_split: { $split: ["$title", " "] },
+    _id: false,
+  } },
+  { $match: {
+    title_split: { $size: 1 },
+  } },
+  { $sort: { title_split: 1 } },
+]);
